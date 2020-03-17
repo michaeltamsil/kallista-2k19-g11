ba25648a9f72b8546ce1c5eb6d434c5d36ebda19
michael tamsil <michaeltamsil@gmail.com>
2020-03-17 10:15:29 +0700

    buat readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# website kalista grade 9
+
+ini utk menampilkan tiap tiap siswa hasil website yang dibuat per minggu / pertemuan
--- script.js
+++ script.js
@@ -3,50 +3,49 @@ $(function () {
     const fnPrintToBody = (obj) => {
         const {htmlSelector, urlData} = obj;
         $.getJSON(urlData, response => {
-                const {
-                    data
-                } = response
+            const {data} = response
 
-                const newData = data.sort((a, b) => {
-                    console.log(a.name < b.name)
-                    return a.name < b.name ? -1 : 1;
-                });
+            const newData = data.sort((a, b) => {
+                console.log(a.name < b.name)
+                return a.name < b.name
+                    ? -1
+                    : 1;
+            });
 
-                newData.forEach(item => {
-                    let tr = $('<tr></tr>')
+            newData.forEach(item => {
+                let tr = $('<tr></tr>')
 
-                    let name = $('<td></td>')
-                    if (item.name) {
-                        $(name).text(item.name)
-                    }
-                    $(tr).append(name);
+                let name = $('<td></td>')
+                if (item.name) {
+                    $(name).text(item.name)
+                }
+                $(tr).append(name);
 
-                    let githubName = $('<td></td>')
-                    if (item.githubName) {
-                        $(githubName).html(`<a target="_blank" href="https://github.com/${item.githubName}">${item.githubName}</a>`)
-                    }
-                    $(tr).append(githubName);
+                let githubName = $('<td></td>')
+                if (item.githubName) {
+                    $(githubName).html(`<a target="_blank" href="https://github.com/${item.githubName}">${item.githubName}</a>`)
+                }
+                $(tr).append(githubName);
 
-                    let basicHTMLURL = $('<td></td>')
-                    let buttonLink = $(`<a class="btn btn-success" target="_blank">website</a>`);
-                    if (item.basicHTMLURL) {
-                        $(buttonLink).attr('href', item.basicHTMLURL);
-                        $(buttonLink).attr('target', "_blank");
-                    }else{
-                        $(buttonLink).addClass('disabled')
-                    }
-                    $(buttonLink).text('website');
-                    $(basicHTMLURL).html(buttonLink)
+                let basicHTMLURL = $('<td></td>')
+                let buttonLink = $(`<a class="btn btn-success" target="_blank">website</a>`);
+                if (item.basicHTMLURL) {
+                    $(buttonLink).attr('href', item.basicHTMLURL);
+                    $(buttonLink).attr('target', "_blank");
+                } else {
+                    $(buttonLink).addClass('disabled')
+                }
+                $(buttonLink).text('website');
+                $(basicHTMLURL).html(buttonLink)
 
-                    $(tr).append(basicHTMLURL);
-                    $(htmlSelector).append(tr);
-                })
-
-                
+                $(tr).append(basicHTMLURL);
+                $(htmlSelector).append(tr);
             })
-        };
+
+        })
+    };
 
     fnPrintToBody({htmlSelector: '#IPA tbody', urlData: 'IPA.json'})
     fnPrintToBody({htmlSelector: '#IPS tbody', urlData: 'IPS.json'})
-        
+
 })
